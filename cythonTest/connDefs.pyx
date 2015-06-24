a3df791641c99934ea034c53b98fe412a306eee2
csinva <csinva@yahoo.com>
2015-06-24 13:42:50 -0400

    runs without crashing

--- cythonTest/connDefs.pyx
+++ cythonTest/connDefs.pyx
@@ -13,7 +13,14 @@ cdef extern from "conn.h":
 
 def arr_test(np.ndarray[np.double_t,ndim=4] conn, np.ndarray[np.double_t,ndim=2] nhood, np.ndarray[np.double_t,ndim=3] comp, list[int] cmpSz):
     # insert in the ascontiguousarray?
+    count = 0
+    for x in range(25):
+        #print count,conn[0][0][0][x]
+        count = count+1
+    #print conn[0,0,0:5,0]
+    #print conn.flatten()[0:10]
     dims = np.shape(conn)
     c = printArr(<double*> conn.data, <double*> nhood.data,dims[0],dims[1],dims[2],<double*> comp.data,<list[int]*> &cmpSz)
     return c
+    #return None
     #printArr(&x[0])
--- mainTest.py
+++ mainTest.py
@@ -3,48 +3,25 @@ import matplotlib.pyplot as plt
 from loadAffs import loadAffs
 import sys
 sys.path.append('cythonTest')
-from test import list_test
-from test import arr_test
-# a = np.array([[1,2,3],[2,3,4]])
-# b = np.array([[1,0,0],[0,1,1]])
-#
-# print (a.flatten()>2)
-# print (b.flatten())
-#
-#       #==b.flatten()
-# #a = np.ravel(a)
-# print a.flatten()
-#
-# print np.sum(b.flatten())
-#
-# initialThresholds = np.arange(-.5,.5,.4)
-# print initialThresholds
+from connDefs import list_test
+from connDefs import arr_test
 
-'''
-def example_plot(ax, fontsize=12):
-    ax.plot([1, 2])
-    ax.locator_params(nbins=3)
-    ax.set_xlabel('x-label', fontsize=fontsize)
-    ax.set_ylabel('y-label', fontsize=fontsize)
-    ax.set_title('Title', fontsize=fontsize)
-
-plt.close('all')
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
-example_plot(ax1)
-example_plot(ax2)
-example_plot(ax3)
-example_plot(ax4)
-plt.tight_layout()
-plt.show()
-'''
 dataRoot = 'dataSmall/000'
 dims = [73,73,73]
 affTrue, affEst = loadAffs(dataRoot,dims)
-compOutput = np.zeros((10,10,10))
+compOutput = np.zeros((73,73,73))
 #list_test(dims)
-print compOutput[0,0,0:5]
-cmpSize = [1,2]
-cmpSize = arr_test(affTrue,np.eye(3),compOutput,cmpSize)
-print "outputs..."
-print compOutput[0,0,0:5]
+#print compOutput[0,0,0:5]
+# affTrue = np.transpose(affTrue) #this will get it to return and then error
+affTrue = np.asfortranarray(affTrue)
+# affTrue = affTrue.astype(dtype='d',order='F')
+# print affEst[0,0,0:5,0]
+nhood = -1*np.eye(3)
+nhood = nhood.astype(dtype='d',order='F')
+cmpSize = []
+cmpSize = arr_test(affTrue,nhood,compOutput,cmpSize)
 print cmpSize
+print "returned"
+# print "outputs..."
+# print compOutput[0,0,0:5]
+# print cmpSize
