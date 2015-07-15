3910ad9049f03c889cd8f80b1871dc6f2388d97d
csinva <csinva@yahoo.com>
2015-07-15 16:05:05 -0400

    working calculation of truePos

--- mainTest.py
+++ mainTest.py
@@ -7,20 +7,25 @@ sys.path.append('watershed')
 from connDefs import connectedComponents
 from waterDefs import markerWatershed
 from randStats import randIndex
-
+import scipy.io as sio
+import time
+'''
 dataRoot = 'dataSmall/000'
 dims = [73,73,73]
 affTrue, affEst = loadAffs(dataRoot,dims)
 comp = np.zeros((73,73,73))
+compTrue = np.zeros((73,73,73))
 affTrue = affTrue.astype(dtype='d',order='F')
-nhood = np.eye(3).astype(dtype='d')
+nhood = np.eye(3).astype(dtype='d',order='F')
 cmpSize = []
+cmpSizeTrue = []
 thresh=.97
 
 affEst = affEst.astype(dtype='d',order='F') # this has an effect
 affEstThresh=(affEst>thresh).astype(dtype='d')
 print "sum affEst:",np.sum(affEstThresh)
 comp,cmpSize = connectedComponents(affEstThresh,nhood,comp,cmpSize)
+compTrue,cmpSizeTrue = connectedComponents(affTrue,nhood,compTrue,cmpSizeTrue)
 LEN = min(10,cmpSize.size)
 print cmpSize[0:LEN-1],'...'
 print "comp sum:",np.sum(comp)
@@ -30,11 +35,13 @@ affEst = affEst.astype(dtype='d',order='F')
 nhood = -1*np.eye(3).astype(dtype='d')
 comp = comp.astype(dtype='d',order='F') #possible that this is already stored in memory as F, shouldn't be converted
 compE = np.zeros((73,73,73)).astype(dtype='d',order='F')
+compT = np.zeros((73,73,73)).astype(dtype='d',order='F')
 print '--------------------ALIGNING---------------------------------------\n'
 for x in range(73):
     for y in range(73):
         for z in range(73):
             compE[z,y,x]=comp[x,y,z]
+            compT[z,y,x]=compTrue[x,y,z]
 # affEst is properly aligned
 print "sum",np.sum(affEst)
 print affEst[0:10,0,0,0]
@@ -53,6 +60,12 @@ watershed = markerWatershed(affEst,nhood,compE,growMask,0,watershed)
 
 print "watershed shape:",watershed.shape
 print "watershed sum:",np.sum(watershed) #this should be around 1020717
+'''
 
 #need to test this with arrays loaded from matlab
-#ri,stats = randIndex()
+# ri,stats = randIndex(compT,compE)
+data = sio.loadmat('matscripts/randSetup.mat')
+compTrue = data['compTrue']
+watershed = data['watershed']
+stats = randIndex(compTrue,watershed)
+print stats
--- randStats.py
+++ randStats.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
 from collections import namedtuple
+from sklearn import metrics
 
 
 def randStatsForThreshold(compTrue, affEst, threshold):
@@ -22,44 +23,76 @@ def randStatsForThreshold(compTrue, affEst, threshold):
 def randIndex(compTrue, compEst, normalize=False):  # todo: this can store less values
     # condition input (also shift to make positive)
     # insert line to make double and positive
-    compTrue = compTrue.flatten()
-    compEst = compEst.flatten()
+    compTrue = compTrue.flatten() #don't have to add 1 for python
+    compEst = compEst.flatten()  #should be 0-indexed
     maxCompTrue = np.max(compTrue)
     maxCompEst = np.max(compEst)
+    print "maxCompTrue:",maxCompTrue
+    print "maxCompEst:",maxCompEst
 
-    # compute the overlap or confusion matrix
-    # this computes the fraction of each true component
-    # overlapped by an estimated component
-    # the sparse() is used to compute a histogram over object pairs
-    overlap = sp.sparse(compTrue, compEst, 1, maxCompTrue, maxCompEst)
+    truePos=0  #it's supposed to be same segment and is
+    falsePos=0 #supposed to be different segment, but is predicted same
+    trueNeg=0
+    falseNeg=0
+    overlap = np.zeros((maxCompTrue+1,maxCompEst+1))
+    print overlap.shape
+    for i in range(compTrue.size):
+        overlap[(compTrue[i],compEst[i])] += 1
+    # print overlap[0][0:10]
+    compTrueSizes = np.sum(overlap[1:,:],axis=1)
+    compEstSizes = np.sum(overlap[1:,1:],axis=0)
+    print "compTrueSizes:",compTrueSizes[0:4]
 
-    # compute the effective sizes of each set of objects
-    # computing it from the overlap matrix normalizes the sizes
-    # of each set of objects to the intersection of assigned space
-    compTrueSizes = np.full(np.sum(overlap[2:-1, :], 2))
-    compEstSizes = np.full(np.sum(overlap[2:-1, 2:-1], 1))
+    # prune out the zero component
+    zeroEst = overlap[1:,1]
+    overlap = overlap[1:,1:]
+    indexes = np.where(overlap)
+    idTrue = indexes[0]
+    idEst = indexes[1]
+    print "indexes shape:",idTrue.shape
+    # print "indexes:",indexes[0,0:4]
+    # print "indexes1:",indexes[1,0:4]
+    # print "maxIndes:",np.max(indexes[:,0])
+    overlapSz = np.sort(overlap[indexes])[::-1]
+    print "overlapSz.shape:",overlapSz.shape
+    print "overlapSz:",overlapSz[0:5]
+
+
+    #not normalizing
+    nPixTotal = np.sum(compTrueSizes)
+    pixSz = 1/nPixTotal
+    fracTrue = compTrueSizes/nPixTotal
+
+    overlapFrac = overlapSz * pixSz
+    overlap = np.zeros((overlap.shape[0]+1,overlap.shape[1]+1))
+    print "overlap shape:",overlap.shape
+    print "max idTrue",np.max(idTrue)
+    for i in range(overlapFrac.size):
+        overlap[idTrue[i],idEst[i]] += overlapFrac[i]
+    print "overlap sum:",np.sum(overlap)
+    print "overlap shape:",overlap.shape
+    #recalculate overlap
+    pos = (np.sum(fracTrue**2) - np.sum(pixSz**2 * compTrueSizes)) / 2 #these don't have to be computed, just = truePos+falseNeg
+    neg = (np.sum(fracTrue)**2-np.sum(fracTrue**2)) / 2
+    total = pos+neg
+    truePos = np.sum(overlapFrac*(overlapFrac-pixSz))/2
+    falsePos = (np.sum(np.sum(overlap,1)**2) - np.sum(np.sum(overlap**2,1)))/2
+
+    print np.sum(overlap,1)
+    #print np.sum(np.sum(overlap**2,1))
+
+    ri = (truePos+trueNeg) / (truePos+trueNeg+falsePos+falseNeg)
+
 
     # derived statistics
-    stats = namedtuple('stats', 'total', 'pos', 'neg', 'prec', 'rec', 'posEst', 'negEst', 'truePos', 'falsePos','trueNeg', 'falseNeg', 'truePosRate', 'falsePosRate', 'mergeRate', 'splitRate','clusteringError')
-    stats.clusteringError = (falsePos + falseNeg) / total
-    ri = 1 - stats.clusteringError
+    stats = {}
+    stats['ri']=ri
+    stats['pos']=pos
+    stats['neg']=neg
+    stats['truePos']=truePos
+    stats['trueNeg']=trueNeg
+    stats['falsePos']=falsePos
+    stats['falseNeg']=falseNeg
+    stats['total']=total
 
-    stats.total = total
-    stats.truePosRate = truePos / pos
-    stats.falsePosRate = falsePos / neg
-    stats.prec = truePos / posEst
-    stats.rec = stats.truePosRate
-    stats.mergeRate = falsePos / pos
-    stats.splitRate = falseNeg / negEst
-
-    stats.truePos = truePos
-    stats.falsePos = falsePos
-    stats.trueNeg = trueNeg
-    stats.falseNeg = falseNeg
-
-    stats.pos = pos
-    stats.neg = neg
-    stats.posEst = posEst
-    stats.negEst = negEst
-
-    return ri, stats
+    return stats
